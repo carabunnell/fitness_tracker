5b9023b647c1b9ee8862858bd4eefd13699da36f
Cara Bunnell <carabunnell@gmail.com>
2020-05-23 08:38:57 -0700

    pushing final routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -38,7 +38,44 @@ router.get("/api/workouts", function (req, res) {
         });
     });
 
-
+    router.post("/api/workouts", ({ body }, res) => {
+        Workout.create(body)
+            .then((data) => {
+            res.json(data);
+            })
+            .catch((err) => {
+            res.status(400).json(err);
+            });
+        });
+        
+        router.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then((data) => res.json(data))
+            .catch((err) => {
+            res.status(400).json(err);
+            });
+        });
+        
+        router.get("/api/workouts/range", function (req, res) {
+        Workout.find()
+            .then((data) => res.json(data))
+            .catch((err) => {
+            res.json(err);
+            });
+        });
+        
+        router.post("/api/workouts/range", function (req, res) {
+        Workout.create({})
+            .then((data) => res.json(data))
+            .catch((err) => {
+            res.json(err);
+            });
+        });
+    
     
     
 
